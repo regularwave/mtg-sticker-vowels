73d4bedd199842b957515f81543858977ce83db5
regularwave <regularwave@gmail.com>
2023-08-30 17:45:42 -0400

    Update stickerVowels.py

--- stickerVowels.py
+++ stickerVowels.py
@@ -1,6 +1,6 @@
 import random
-iterations, j = 1000000, 0
-for draws in range(0, iterations):
+iterations, j = 1234567, 0
+for draws in range(iterations):
     stickerVowels = [6, 5, 5, 4, 4, 4, 4, 4, 4, 3]
     random.shuffle(stickerVowels)
     if 3 in stickerVowels[-3::]:
